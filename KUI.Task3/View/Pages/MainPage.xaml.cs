cb960ccd954f1b04a92c5e645332fb4e607619d9
Пользователь <@DESKTOP-KVSP5S9>
2023-02-21 09:30:29 +0300

    Добавьте файлы проекта.

---
+++ KUI.Task3/View/Pages/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using KUI.Task3.Core;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KUI.Task3.View.Pages
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTask1_Click(object sender, RoutedEventArgs e)
+        {
+            Test.TestFrame?.Navigate(new Task1());
+        }
+    }
+}
---
+++ KUI.Task3/View/Windows/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using KUI.Task3.Core;
+using KUI.Task3.View.Pages;
+using System.Windows;
+
+namespace KUI.Task3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Test.TestFrame = MainFrame;
+            MainFrame.Navigate(new MainPage());
+        }
+    }
+}
